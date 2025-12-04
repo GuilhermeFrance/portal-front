ad38f750e471ec7e2b164ff415ae7f9e5dd53c3a
Guilherme <guilhermefrance9@gmail.com>
2025-12-04 16:19:13 -0300

    finally-dashboard

--- src/components/DashboardComponent.vue
+++ src/components/DashboardComponent.vue
@@ -28,7 +28,7 @@ async function handleDashboardInfos() {
 
 function createChart() {
   const ctx = document.getElementById("myChart") as HTMLCanvasElement;
-  
+
   if (ctx && Requests.value.length > 0) {
     // Destroi o chart anterior se existir
     if (chartInstance) {
@@ -38,13 +38,13 @@ function createChart() {
     const config = {
       type: "doughnut" as const,
       data: {
-        labels: Requests.value.map((item) => item.statusKey),
+        labels: Requests.value.map((item) => item.statusKey.toUpperCase()),
         datasets: [
           {
-            label: "Requests por Status",
+            label: " Quantidade ",
             data: Requests.value.map((item) => item._count),
             borderWidth: 1,
-            backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#4BC0C0"],
+            backgroundColor: ["#eaa468", "#6897ea", "#68ea76", "#ea6868"],
             borderColor: "#ffffff",
             hoverOffset: 4,
           },
@@ -62,8 +62,8 @@ function createChart() {
             position: "bottom" as const,
           },
         },
-        cutout: "60%",
-        radius: "80%",
+        cutout: "70%",
+        radius: "90%",
       },
     };
 
@@ -72,11 +72,15 @@ function createChart() {
 }
 
 // Watch para reagir às mudanças nos dados
-watch(Requests, (newRequests) => {
-  if (newRequests.length > 0) {
-    createChart();
-  }
-}, { deep: true });
+watch(
+  Requests,
+  (newRequests) => {
+    if (newRequests.length > 0) {
+      createChart();
+    }
+  },
+  { deep: true }
+);
 
 onMounted(() => {
   handleDashboardInfos();
@@ -86,8 +90,28 @@ onMounted(() => {
 <template>
   <section>
     <div class="main-content">
-      <div>
-        <canvas id="myChart"> </canvas>
+      <span class="title">Dashboard</span>
+      <div class="boxDashboard">
+        <div class="dashboard">
+          <canvas id="myChart"> </canvas>
+        </div>
+        <div class="modal-dashboard">
+          <div class="type-card" v-for="request in Requests">
+            <div class="title-status">
+              <span>Status:</span>
+              <span :class="{
+                        isOpen: request.statusKey === 'aberto',
+                        Processing: request.statusKey === 'processando',
+                        Conclused: request.statusKey === 'concluido',
+                        Rejected: request.statusKey === 'rejeitado',
+                      }">{{ request.statusKey.toUpperCase() }}</span>
+            </div>
+             <div class="title-status">
+              <span>Quantidade:</span>
+              <span>{{ request._count }}</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -106,11 +130,77 @@ section {
 
 .main-content {
   display: flex;
+  flex-direction: column;
+  justify-content: center;
   padding: 20px;
-  width: 1200px;
+  width: 1100px;
   height: 600px;
   background-color: rgb(255, 255, 255);
   border-radius: 30px;
   box-shadow: 2px 2px 32px 2px rgba(66, 66, 66, 0.068);
+}
+
+.title {
+  font-size: 40px;
+  font-weight: 600;
+  padding-left: 20px;
+}
+
+.dashboard {
+  padding-top: 60px;
+  width: 400px;
+}
+.boxDashboard {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  gap: 100px;
+}
+.modal-dashboard {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 500px;
+  padding: 10px;
+  gap: 10px;
+  height: 400px;
+  background-color: rgb(255, 255, 255);
+  border-radius: 30px;
+  box-shadow: 1px 1px 15px 1px rgb(228, 228, 228);
+}
+.type-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 40px;
+  width: 400px;
+  height: 80px;
+  background-color: rgb(255, 255, 255);
+  border: 1px solid rgb(230, 230, 230);
+  border-radius: 20px;
+  transition: 0.3s ease;
+}
+.type-card:hover{
+  background-color: rgba(230, 230, 230, 0.541);
+}
+.title-status{
+  display: flex;
+  gap: 10px;
+  font-weight: 300;
+}
+
+.isOpen {
+  color: rgb(255, 153, 0);
+}
+.Processing {
+  color: rgb(0, 140, 233);
+}
+.Conclused {
+  color: green;
+}
+.Rejected {
+  color: rgb(189, 0, 0);
 }
 </style>
